e3362634d52d7c15d1ee44441c6aa2f716733541
cpadwick <chris.padwick@bluerivert.com>
2018-12-27 09:22:13 -0800

    add a plot to plot_repeated_runs

--- src/plot_repeated_runs.py
+++ src/plot_repeated_runs.py
@@ -8,15 +8,23 @@ allruns = api.runs('cpadwick/airbus_ships')
 
 result = []
 for arun in allruns:
-    if '_runtime' in arun.summary.keys():
+    if arun.state == 'finished':
         # Runtime for the runs of interest was between 4 and 5 hours.
-        if arun.summary['_runtime'] > 28800.0 and arun.summary['_runtime'] < 32000:
-            result.append({'max_fscore_fg': arun.summary['max_fscore_fg']})
+        if arun.summary['_runtime'] > 28800.0 and arun.summary['_runtime'] < 34000:
+            result.append({'max_fscore_fg': arun.summary['max_fscore_fg'],
+                           'loss': arun.summary['loss']})
 
 df = pd.DataFrame(result)
 
-ax = sns.violinplot(data=df)
+plt.figure(1)
+ax = sns.violinplot(y='max_fscore_fg', data=df)
 ax.set_title('Violin Plot of F-Score of Repeated Runs')
+plt.show()
+
+plt.figure(2)
+
+ax = sns.scatterplot(x=df['loss'], y=df['max_fscore_fg'])
+ax.set_title('Scatter Plot of Loss vs Fscore')
 plt.show()
 
 
